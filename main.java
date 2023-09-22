926dc9d42ff31cc447a8eb68fb7d8d325c171e8f
Stas <www.stas1996@mail.ru>
2023-09-22 09:47:16 +0300

    main

---
+++ main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        ToyStore store = new ToyStore();
+        store.addToy(new Toy(1, "Медведь", 10, 30.0));
+        store.addToy(new Toy(2, "Кукла", 15, 50.0));
+        store.addToy(new Toy(3, "Машина", 5, 20.0));
+        store.drawPrize();
+        Toy prize = store.getPrize();
+        System.out.println("Призовая игрушка: " + String.valueOf(prize));
+    }
+}
