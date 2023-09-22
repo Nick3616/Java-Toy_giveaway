6e591ebc90adde17552661b1f589bce5f2d6e308
Stas <www.stas1996@mail.ru>
2023-09-22 09:48:20 +0300

    Toy

---
+++ Toy.java
@@ -0,0 +1,41 @@
+class Toy {
+    private int id;
+    private String name;
+    private int quantity;
+    private double weight;
+
+    public Toy(int id, String name, int quantity, double weight) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.weight = weight;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
